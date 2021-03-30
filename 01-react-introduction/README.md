274d5da62d8a9114944cdbae596b94d599545a6d
ywarezk <ywarezk@gmail.com>
2021-03-30 09:57:05 +0300

    added the first lesson

--- 01-react-introduction/README.md
+++ 01-react-introduction/README.md
@@ -1,9 +1,28 @@
 # React introduction
 
+## Open a static server
+
+```
+> npx http-server
+```
+
+```
+> npx @angular/cli new some-project
+```
+
+```
+> npm install <some-package>
+```
+
+
 ## Lesson Plan
 
 1. hello world
 2. virtual dom
 3. components
 4. jsx
+
+## Video
+
+https://www.youtube.com/watch?v=bVPMCmTN__Q&t=1s
 
---
+++ 01-react-introduction/hello.js
@@ -0,0 +1,76 @@
+
+/*
+const h1 = document.createElement('h1');
+h1.innerText = 'Hello world with javascript';
+
+const container = document.getElementById('container');
+container.appendChild(h1);
+*/
+
+/**
+ * 
+ 
+ js ==========> DOM
+ 
+ virtual dom with React.createElement =====> ReactDOM ======> DOM
+ virtual dom with React.createElement =====> react native ========> mobile app
+ virtual dom with React.createElement =====> Electron ========> desktop
+ 
+ React core ====> Platform specific
+ 
+ */
+
+/*
+
+jsx
+
+<h1></h1>
+
+*/
+/*
+const h1 = React.createElement(
+	'h1',
+	null,
+	React.createElement(
+		'span',
+		null,
+		React.createElement(
+			'u',
+			null,
+			'hello world with react and underline'
+		)
+	)
+)
+*/
+
+/*
+const h1 = (
+	<h1>
+		<span>
+			<u>
+				hello world with react and underline and jsx
+			</u>
+		</span>
+	</h1>
+)
+*/
+
+// Components
+function HelloWorld() {
+	return (
+		<h1>
+			<span>
+				<u>
+					hello world with react and underline and jsx in function
+				</u>
+			</span>
+		</h1>
+	)
+}
+
+
+ReactDOM.render(
+	// React.createElement(HelloWorld)
+	<HelloWorld />,
+	document.getElementById('container')
+)
